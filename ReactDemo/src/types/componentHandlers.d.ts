29b807a7db02dd2b13835b4553de8ebf3d3449fc
Justin Bush <jdevbush@gmail.com>
2024-06-07 15:15:37 -0600

    refactor: rm template code

--- ReactDemo/src/types/componentHandlers.d.ts
+++ ReactDemo/src/types/componentHandlers.d.ts
@@ -14,23 +14,4 @@ export interface ComponentHandlers {
   updateSwitch: {
     postMessage: (value: boolean) => void;
   };
-  // TODO: Insert message handlers here to communicate with Swift app through WKWebView.
-  // Some example syntax:
-  /*
-  userLoggedIn: {
-    sendMessage: (data: { userId: number; timestamp: string }) => void;
-  };
-  userLoggedOut: {
-    notify: (status: boolean) => void;
-  };
-  cartUpdated: {
-    updateCart: (items: { itemId: number; quantity: number }[]) => void;
-  };
-  notificationReceived: {
-    alertUser: (notification: { title: string; body: string }) => void;
-  };
-  customMessageHandler: {
-    sendObjectToSwiftApp: (data: { key: string; value: any }) => void;
-  };
-  */
 }
