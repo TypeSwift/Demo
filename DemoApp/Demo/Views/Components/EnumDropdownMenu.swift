d08437757c49b726faacc355d67a81665074389f
Justin Bush <jdevbush@gmail.com>
2024-06-07 10:40:51 -0600

    feat: TypeSwift-generated, spread components

---
+++ DemoApp/Demo/Views/Components/EnumDropdownMenu.swift
@@ -0,0 +1,48 @@
+//
+//  EnumDropdownMenu.swift
+//  Demo
+//
+//  Created by Justin Bush on 6/7/24.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct EnumDropdownMenu<EnumType>: View where EnumType: RawRepresentable, EnumType: CaseIterable, EnumType.RawValue == String, EnumType: Hashable {
+  @Binding var selection: EnumType
+  @Environment(\.colorScheme) var colorScheme
+  
+  var body: some View {
+    Picker("", selection: $selection) {
+      ForEach(Array(EnumType.allCases), id: \.self) { value in
+        Text(value.rawValue).tag(value)
+      }
+    }
+    .controlSize(.extraLarge)
+    .pickerStyle(.menu)
+    .padding(.horizontal)
+    .padding(.vertical, 10)
+    .frame(maxWidth: .infinity)
+    .frame(height: 54)
+    .background(
+      colorScheme == .dark
+      ? Color(0x606463)
+      : Color(0xEDEDED)
+    )
+    .cornerRadius(8)
+    .shadow(color: Color.black.opacity(0.6), radius: 0.5, x: 0, y: 1)
+  }
+}
+
+
+#Preview {
+  @State var selectedDevice: TypeSwift.Device = .Mac
+  @State var selectedOS: TypeSwift.OperatingSystem = .macOS
+  
+  return VStack {
+    EnumDropdownMenu(selection: $selectedDevice)
+    EnumDropdownMenu(selection: $selectedOS)
+  }
+  .padding()
+}
