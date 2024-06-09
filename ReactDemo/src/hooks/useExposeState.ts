02763e39bab58891627043a936557da98bf7a9c0
Justin Bush <jdevbush@gmail.com>
2024-06-09 12:00:41 -0600

    refactor: argument overrides for empty undefined

--- ReactDemo/src/hooks/useExposeState.ts
+++ ReactDemo/src/hooks/useExposeState.ts
@@ -8,17 +8,33 @@ import {
 } from 'react';
 
 function useExposeState<T>(
+  key: string
+): [T | undefined, Dispatch<SetStateAction<T | undefined>>];
+function useExposeState<T>(
   initialValue: T,
   key: string
-): [T, Dispatch<SetStateAction<T>>] {
-  const [state, _setState] = useState<T>(initialValue);
+): [T, Dispatch<SetStateAction<T>>];
+
+function useExposeState<T>(...args: [string] | [T, string]) {
+  let initialValue: T | undefined;
+  let key: string;
+
+  if (args.length === 1) {
+    initialValue = undefined;
+    key = args[0];
+  } else {
+    initialValue = args[0];
+    key = args[1];
+  }
+
+  const [state, _setState] = useState<T | undefined>(initialValue);
 
   const stateRef = useRef(state);
   stateRef.current = state;
 
   const handler = useMemo(
     () => ({
-      set: (obj: any, prop: string, value: T) => {
+      set: (obj: any, prop: string, value: T | undefined) => {
         if (prop === 'value') {
           _setState(value);
           if (window.webkit?.messageHandlers?.[key]) {
@@ -42,12 +58,12 @@ function useExposeState<T>(
     (window as any)[key] = stateProxy;
   }, [key, stateProxy]);
 
-  const setState: Dispatch<SetStateAction<T>> = (newValue) => {
+  const setState: Dispatch<SetStateAction<T | undefined>> = (newValue) => {
     if (typeof newValue === 'function') {
-      const valueFn = newValue as (prevState: T) => T;
+      const valueFn = newValue as (prevState: T | undefined) => T | undefined;
       stateProxy.value = valueFn(stateRef.current);
     } else {
-      stateProxy.value = newValue as T;
+      stateProxy.value = newValue;
     }
   };
 
