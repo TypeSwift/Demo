ecc6e07a04fb7016202d092d67efa47adfcc2cb9
Justin Bush <jdevbush@gmail.com>
2024-06-09 10:49:52 -0600

    refactor: properly intercept state change, set proxy state

--- ReactDemo/src/hooks/useExposeState.ts
+++ ReactDemo/src/hooks/useExposeState.ts
@@ -1,16 +1,26 @@
-import { useState, useEffect, useMemo } from 'react';
+import {
+  useState,
+  useEffect,
+  useMemo,
+  useRef,
+  Dispatch,
+  SetStateAction,
+} from 'react';
 
 function useExposeState<T>(
   initialValue: T,
   key: string
-): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [state, setState] = useState<T>(initialValue);
+): [T, Dispatch<SetStateAction<T>>] {
+  const [state, _setState] = useState<T>(initialValue);
+
+  const stateRef = useRef(state);
+  stateRef.current = state;
 
   const handler = useMemo(
     () => ({
       set: (obj: any, prop: string, value: T) => {
         if (prop === 'value') {
-          setState(value);
+          _setState(value);
           if (window.webkit?.messageHandlers?.[key]) {
             window.webkit.messageHandlers[key].postMessage(value);
           } else {
@@ -24,13 +34,22 @@ function useExposeState<T>(
   );
 
   const stateProxy = useMemo(
-    () => new Proxy({ value: state }, handler),
-    [state, handler]
+    () => new Proxy({ value: stateRef.current }, handler),
+    [stateRef, handler]
   );
 
   useEffect(() => {
     (window as any)[key] = stateProxy;
   }, [key, stateProxy]);
+
+  const setState: Dispatch<SetStateAction<T>> = (newValue) => {
+    if (typeof newValue === 'function') {
+      const valueFn = newValue as (prevState: T) => T;
+      stateProxy.value = valueFn(stateRef.current);
+    } else {
+      stateProxy.value = newValue as T;
+    }
+  };
 
   return [state, setState];
 }
