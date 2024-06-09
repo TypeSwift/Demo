0b0f97ad89cde825a43dab1ca8ffc0e8c23810fa
Justin Bush <jdevbush@gmail.com>
2024-06-09 09:41:13 -0600

    refactor: working postMessage script handler

--- ReactDemo/src/hooks/useExposeState.ts
+++ ReactDemo/src/hooks/useExposeState.ts
@@ -6,26 +6,28 @@ function useExposeState<T>(
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
   const [state, setState] = useState<T>(initialValue);
 
-  // proxy handler to intercept changes
   const handler = useMemo(
     () => ({
       set: (obj: any, prop: string, value: T) => {
         if (prop === 'value') {
           setState(value);
+          if (window.webkit?.messageHandlers?.[key]) {
+            window.webkit.messageHandlers[key].postMessage(value);
+          } else {
+            console.warn(`Message handler '${key}' is not available.`);
+          }
         }
         return true;
       },
     }),
-    []
+    [key]
   );
 
-  // create a proxy for state
   const stateProxy = useMemo(
     () => new Proxy({ value: state }, handler),
     [state, handler]
   );
 
-  // expose the proxy to the global scope
   useEffect(() => {
     (window as any)[key] = stateProxy;
   }, [key, stateProxy]);
